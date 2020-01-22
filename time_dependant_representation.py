3509a4ac091e8c70950aef63aa983795a3dd0d4d
Charles Monzani <cmonzani@tradelab.fr>
2020-01-22 22:52:50 +0100

    Implementation of time_masking

--- time_dependant_representation.py
+++ time_dependant_representation.py
@@ -7,9 +7,11 @@ from tensorflow.keras import initializers, regularizers, constraints, activation
 
 import numpy as np
 from tensorflow.keras import backend as K
+from math import log
 
 class TimeDepMaskingCell(Layer):
     def __init__(self, units,
+                 activation='tanh',
                  recurrent_activation='sigmoid',
                  use_bias=True,
                  kernel_initializer='glorot_uniform',
@@ -27,16 +29,195 @@ class TimeDepMaskingCell(Layer):
                  implementation=2,
                  **kwargs
                  ):
-        pass
+        super(TimeDepMasking, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.recurrent_activation = activations.get(recurrent_activation)
+        self.time_mask_activation = activations.get('sigmoid')
+        self.use_bias = use_bias
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.unit_forget_bias = unit_forget_bias
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        self.dropout = min(1., max(0., dropout))
+        self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+        self.implementation = implementation
+        self.state_size = (self.units, self.units)
+        self.output_size = self.units
+        self._dropout_mask = None
+        self._recurrent_dropout_mask = None
 
     def build(self, input_shape):
-        pass
+        input_dim = input_shape[-1]
+        assert self.units >= input_dim
+
+        if type(self.recurrent_initializer).__name__ == 'Identity':
+            def recurrent_identity(shape, gain=1., dtype=None):
+                del dtype
+                return gain * np.concatenate(
+                    [np.identity(shape[0])] * (shape[1] // shape[0]), axis=1)
+
+            self.recurrent_initializer = recurrent_identity
+
+        self.kernel = self.add_weight(shape=(input_dim, self.units * 5),
+                                      name='kernel',
+                                      initializer=self.kernel_initializer,
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 4),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint)
+
+        if self.use_bias:
+            if self.unit_forget_bias:
+
+                def bias_initializer(_, *args, **kwargs):
+                    return K.concatenate([
+                        self.bias_initializer((self.units,), *args, **kwargs),
+                        initializers.Ones()((self.units,), *args, **kwargs),
+                        self.bias_initializer((self.units * 3,), *args, **kwargs),
+                    ])
+            else:
+                bias_initializer = self.bias_initializer
+            self.bias = self.add_weight(shape=(self.units * 5,),
+                                        name='bias',
+                                        initializer=bias_initializer,
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+
+        self.kernel_i = self.kernel[:, :self.units]
+        self.kernel_f = self.kernel[:, self.units: self.units * 2]
+        self.kernel_c = self.kernel[:, self.units * 2:self.units * 3]
+        self.kernel_o = self.kernel[:, self.units * 3:self.units * 4]
+        self.kernel_t = self.kernel[:, self.units * 4: self.units * 5]
+
+        self.recurrent_kernel_i = self.recurrent_kernel[:, :self.units]
+        self.recurrent_kernel_f = (self.recurrent_kernel[:, self.units: self.units * 2])
+        self.recurrent_kernel_c = (self.recurrent_kernel[:, self.units * 2:self.units * 3])
+        self.recurrent_kernel_o = self.recurrent_kernel[:, self.units * 3: self.units * 4]
+
+        if self.use_bias:
+            self.bias_i = self.bias[:self.units]
+            self.bias_f = self.bias[self.units: self.units * 2]
+            self.bias_c = self.bias[self.units * 2: self.units * 3]
+            self.bias_o = self.bias[self.units * 3:self.units * 4]
+            self.bias_t = self.bias[self.units * 4: self.units * 5]
+
+        else:
+            self.bias_i = None
+            self.bias_f = None
+            self.bias_t = None
+            self.bias_c = None
+            self.bias_o = None
+
+        self.built = True
     
     def call(self, inputs, states, training=None):
-        pass
+        if 0 < self.dropout < 1 and self._dropout_mask is None:
+            self._dropout_mask = _generate_dropout_mask(
+                K.ones_like(inputs),
+                self.dropout,
+                training=training,
+                count=5)
+        if (0 < self.recurrent_dropout < 1 and
+                self._recurrent_dropout_mask is None):
+            self._recurrent_dropout_mask = _generate_dropout_mask(
+                K.ones_like(states[0]),
+                self.recurrent_dropout,
+                training=training,
+                count=5)
+
+        # dropout matrices for input units
+        dp_mask = self._dropout_mask
+        # dropout matrices for recurrent units
+        rec_dp_mask = self._recurrent_dropout_mask
+
+        h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
+
+        # inputs: [event_dimension + 1]
+
+        x_t = inputs[:, :-1]
+        dt = inputs[:, -1]
+
+        c_d = log(dt)
+        c_d = dt * self.kernel[-1, self.units * 4:self.units * 5]
+
+        m_d = self.time_mask_activation(c_d)
+        x_t = tf.multiply(m_d, x_t)
+
+
+        if 0. < self.dropout < 1.:
+            x_t *= dp_mask[0]
+        z = K.dot(x_t, self.kernel[:-1, :self.units * 5])
+        if 0. < self.recurrent_dropout < 1.:
+            h_tm1 *= rec_dp_mask[0]
+        z += K.dot(h_tm1, self.recurrent_kernel[:, :self.units * 5])
+        if self.use_bias:
+            z = K.bias_add(z, self.bias[:self.units * 5])
+
+        dt_aux = dt * self.recurrent_kernel[-1, self.units * 5:]
+
+        z0 = z[:, :self.units]
+        z1 = z[:, self.units: 2 * self.units]
+        z2 = z[:, 2 * self.units: 3 * self.units]
+        z3 = z[:, 3 * self.units: 4 * self.units]
+        z4 = z[:, 4 * self.units: 5 * self.units]
+
+        i = self.recurrent_activation(z0)
+        f = self.recurrent_activation(z1)
+        t = self.time_activation(z2 + dt_aux)
+        c = f * c_tm1 + i * t * self.activation(z3)
+        o = self.recurrent_activation(z4)
+        h = o * self.activation(c)
+
+        if 0 < self.dropout + self.recurrent_dropout:
+            if training is None:
+                h._uses_learning_phase = True
+        return h, [h, c]
 
     def get_config(self):
-        pass
+        config = {'units': self.units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation':
+                      activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer':
+                      initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer':
+                      initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'unit_forget_bias': self.unit_forget_bias,
+                  'kernel_regularizer':
+                      regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer':
+                      regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint':
+                      constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(TimeDepMaskingCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 class TimeDepMasking(RNN):
 
@@ -64,13 +245,52 @@ class TimeDepMasking(RNN):
                  stateful=False,
                  unroll=False,
                  **kwargs):
-        pass
+        if implementation == 0:
+            warnings.warn('`implementation=0` has been deprecated, '
+                          'and now defaults to `implementation=1`.'
+                          'Please update your layer call.')
+        if K.backend() == 'theano' and (dropout or recurrent_dropout):
+            warnings.warn(
+                'RNN dropout is no longer supported with the Theano backend '
+                'due to technical limitations. '
+                'You can either set `dropout` and `recurrent_dropout` to 0, '
+                'or use the TensorFlow backend.')
+            dropout = 0.
+            recurrent_dropout = 0.
 
-    def build(self, input_shape):
-        pass
+        cell = TimeDepMaskingCell(units,
+                              activation=activation,
+                              recurrent_activation=recurrent_activation,
+                              use_bias=use_bias,
+                              kernel_initializer=kernel_initializer,
+                              recurrent_initializer=recurrent_initializer,
+                              unit_forget_bias=unit_forget_bias,
+                              bias_initializer=bias_initializer,
+                              kernel_regularizer=kernel_regularizer,
+                              recurrent_regularizer=recurrent_regularizer,
+                              bias_regularizer=bias_regularizer,
+                              kernel_constraint=kernel_constraint,
+                              recurrent_constraint=recurrent_constraint,
+                              bias_constraint=bias_constraint,
+                              dropout=dropout,
+                              recurrent_dropout=recurrent_dropout,
+                              implementation=implementation)
+        super(TimeDepMasking, self).__init__(cell,
+                                         return_sequences=return_sequences,
+                                         return_state=return_state,
+                                         go_backwards=go_backwards,
+                                         stateful=stateful,
+                                         unroll=unroll,
+                                         **kwargs)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
 
     def call(self, inputs, mask=None, training=None, initial_state=None):
-        pass
+        self.cell._dropout_mask = None
+        self.cell._recurrent_dropout_mask = None
+        return super(TimeDepMasking, self).call(inputs,
+                                            mask=mask,
+                                            training=training,
+                                            initial_state=initial_state)
 
     @property
     def units(self):
@@ -141,7 +361,34 @@ class TimeDepMasking(RNN):
         return self.cell.implementation
 
     def get_config(self):
-        pass
+        config = {'units': self.units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation':
+                      activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer':
+                      initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer':
+                      initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'unit_forget_bias': self.unit_forget_bias,
+                  'kernel_regularizer':
+                      regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer':
+                      regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'activity_regularizer':
+                      regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint':
+                      constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(TimeDepMasking, self).get_config()
+        del base_config['cell']
+        return dict(list(base_config.items()) + list(config.items()))
 
     @classmethod
     def from_config(cls, config):
