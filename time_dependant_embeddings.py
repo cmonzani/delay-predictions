7159a6528a898e2e741cbe98fd2c2b8719e4671f
Charles Monzani <cmonzani@tradelab.fr>
2020-02-25 17:36:54 +0100

    WIP Time-Dep embeddings

---
+++ time_dependant_embeddings.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import initializers, regularizers, constraints, activations
+from tensorflow.keras.layers import Layer
+
+
+class time_joint_embedding(Layer):
+
+    def __init__(self,
+                 embedding_size=10,
+                 time_embedding_size=10,
+                 projection_size=10,
+                 time_initializer='glorot_uniform',
+                 time_regularizer=None,
+                 time_constraint=None,
+                 ):
+        super(time_joint_embedding, self).__init__()
+
+        self.time_embedding_size = time_embedding_size
+        self.embedding_size = embedding_size
+        self.projection_size = projection_size
+        self.time_activation = activations.get('sigmoid')
+        self.time_regularizer = regularizers.get(time_regularizer)
+        self.time_initializer = initializers.get(time_initializer)
+        self.time_constraint = constraints.get(time_constraint)
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        input_dim -= 1
+
+        self.input_embedding = self.add_weight(shape=(input_dim, self.embedding_size),
+                                               name='embedding_matrix',
+                                               trainable=True
+                                               )
+
+        self.time_kernel = self.add_weight(shape=(1, self.projection_size),
+                                           name='time_kernel',
+                                           initializer=self.time_initializer,
+                                           regularizer=self.time_regularizer,
+                                           constraint=self.time_constraint,
+                                           trainable=True)
+
+        self.embedding_projector = self.add_weight(shape=(self.projection_size, self.embedding_size),
+                                                   name='time_projection_onto_embedding_space',
+                                                   initializer=self.time_initializer,
+                                                   regularizer=self.time_regularizer,
+                                                   constraint=self.time_constraint,
+                                                   trainable=True
+                                                   )
+
+        self.time_bias = self.add_weight(shape=(self.projection_size,),
+                                         name='time_bias')
+        self.built = True
+
+    def call(self, inputs):
+        dt = inputs[:, :, -1]
+        # dt = tf.reshape(dt, (-1,1))
+        dt = tf.expand_dims(dt, -1)
+        p_d = dt * self.time_kernel
+        p_d = K.bias_add(p_d, self.time_bias)
+
+        # aux_mat = self.time_activation(p_d)
+
+        # s_d = aux_mat/tf.reshape(tf.reduce_sum(aux_mat, axis=1), (-1,1))
+        s_d = tf.nn.softmax(p_d)
+
+        g_d = K.dot(s_d, self.embedding_projector)
+
+        x_t = inputs[:, :, :-1]
+
+        x_t = K.dot(x_t, self.input_embedding)
+        x_t = tf.add(x_t, g_d) / 2
+
+        return x_t
